0480e880c68933a56bbc376969eb185ebc2b9d9f
Shylesh <shylesh.s@ansrsource.com>
2023-07-31 19:07:02 +0530

    data from folder

--- pages/api/shylesh.js
+++ pages/api/shylesh.js
@@ -1,3 +1,27 @@
+import fs from "fs";
+import path from "path";
+
 export default function handler(req, res) {
-  res.status(200).json({ name: "Shylesh" });
+  // Check if the request is for the specific route you want to handle
+  if (req.method === "GET") {
+    const jsonFilePath = path.join(process.cwd(), "public", "data.json");
+
+    fs.readFile(jsonFilePath, "utf8", (err, data) => {
+      if (err) {
+        console.error("Error reading JSON file:", err);
+        res.status(500).json({ error: "Failed to read JSON file" });
+      } else {
+        try {
+          const jsonData = JSON.parse(data);
+          res.status(200).json(jsonData);
+        } catch (parseError) {
+          console.error("Error parsing JSON:", parseError);
+          res.status(500).json({ error: "Failed to parse JSON data" });
+        }
+      }
+    });
+  } else {
+    // If the route doesn't match, return a 404 Not Found response
+    res.status(404).json({ error: "Not Found" });
+  }
 }
